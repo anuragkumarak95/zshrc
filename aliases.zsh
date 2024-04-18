4aed478b3a43a6bbb92219601d68c688855aa2b5
Anurag Kumar <ankumar@sonicwall.com>
2024-04-18 11:25:11 +0530

    adding few more aliases

--- aliases.zsh
+++ aliases.zsh
@@ -5,7 +5,14 @@ alias cat="bat -pp"
 
 # conda alias
 alias c="conda"
+alias ca="conda activate"
 
 # neovim aliasing
-alias vi='nvim'
-alias nv='nvim'
+alias vi="nvim"
+alias nv="nvim"
+
+# kubectl aliases
+alias k="kubectl"
+
+# brew aliases
+alias b="brew"
